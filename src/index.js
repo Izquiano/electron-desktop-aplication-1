d669e6c03405f63ac84277718a3950cbba0752f4
Izquiano <izquiano@hotmail.com>
2020-11-16 17:50:41 +0100

    Menu created

--- src/index.js
+++ src/index.js
@@ -1,62 +1,107 @@
-const { app, BrowserWindow, Menu } = require('electron')
+const { app, BrowserWindow, Menu } = require("electron");
 
-const url = require('url')
-const path = require('path')
+const url = require("url");
+const path = require("path");
 
-if(process.env.NODE_ENV !== 'production'){
-  require('electron-reload')(__dirname, {
-    electron: path.join(__dirname, '../node_modules', 'bin', 'electron')
-  })
+const isMac = process.platform === "darwin";
+
+if (process.env.NODE_ENV !== "production") {
+  require("electron-reload")(__dirname, {
+    electron: path.join(__dirname, "../node_modules", "bin", "electron"),
+  });
 }
 
+let mainWindow;
+let newProductWindow;
 
-let mainWindow
-let newProductWindow
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({
+    title: "Main window",
+  });
 
-app.on('ready', () => {
-  
-  mainWindow = new BrowserWindow({})
-  
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, '/views/index.html'),
-    protocol: 'file',
-    slashes: true
-  }))
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "/views/index.html"),
+      protocol: "file",
+      slashes: true,
+    })
+  );
 
-  const mainMenu = Menu.buildFromTemplate(templateMenu)
-  Menu.setApplicationMenu(mainMenu)
-  mainWindow.on('closed', () => {
-    app.quit()
-  })
-})
+  const mainMenu = Menu.buildFromTemplate(templateMenu);
+  Menu.setApplicationMenu(mainMenu);
+  mainWindow.on("closed", () => {
+    app.quit();
+  });
+});
 
 function createNewProductWindow() {
-  newProductWindow = new BrowserWindow ({
+  newProductWindow = new BrowserWindow({
     width: 400,
     height: 320,
-    title: 'Add a new product'
-  })
-  newProductWindow.setMenu(null)
+    title: "Add a new product",
+  });
+  newProductWindow.setMenu(null);
 
-  newProductWindow.loadURL(url.format({
-    pathname: path.join(__dirname, '/views/new-product.html'),
-    protocol: 'file',
-    slashes: true
-  }))
+  newProductWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "/views/new-product.html"),
+      protocol: "file",
+      slashes: true,
+    })
+  );
 
+  newProductWindow.on("close", () => {
+    newProductWindow = null;
+  });
 }
 
 const templateMenu = [
   {
-    label: 'File',
+    label: "Products",
     submenu: [
       {
-        label: 'New product',
-        accelerator: 'Ctrl+N',
+        label: "New product",
+        accelerator: isMac ? "command+N" : "Ctrl+N",
         click() {
-          createNewProductWindow()
+          createNewProductWindow();
+        },
+      },
+      { type: 'separator' },
+      {
+        label: "Remove all products",
+      },
+    ],
+  },
+];
+
+if (isMac) {
+  templateMenu.unshift({
+    label: app.getName(),
+    submenu: [
+      {
+        label: "Exit",
+        accelerator: isMac ? "command+Q" : "Ctrl+Q",
+        click() {
+          app.quit();
+        },
+      },
+    ],
+  });
+}
+
+if(process.env.NODE_ENV !== 'production'){
+  templateMenu.push({
+    label: 'DevTools',
+    submenu: [
+      {
+        label: 'Show/Hide Dev Tools',
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools()
         }
+      },
+      {
+        role: 'reload'
       }
     ]
-  }
-]
+  })
+}
